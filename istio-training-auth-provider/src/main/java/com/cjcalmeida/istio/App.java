9a9ff7e2ea3790e76b031a4bb72c590fd66e33bb
Cristiano Almeida <cicbboyzb@gmail.com>
2019-12-15 02:38:40 -0300

    Finalizado Auth Provider

--- istio-training-auth-provider/src/main/java/com/cjcalmeida/istio/App.java
+++ istio-training-auth-provider/src/main/java/com/cjcalmeida/istio/App.java
@@ -1,20 +1,25 @@
 package com.cjcalmeida.istio;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -27,54 +32,82 @@ public class App {
 
     @Configuration
     @EnableAuthorizationServer
-    public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter{
+    public static class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
         private AuthenticationManager manager;
+        private PasswordEncoder encoder;
 
-        @Autowired
-        public AuthorizationServerConfiguration(AQualifier("authenticationManagerBean") thenticationManager manager){
-            this.manager = manager;
+        @Value("${auth.private-key:s3cr3t}")
+        private String signingKey;
+
+        public AuthorizationServerConfiguration(AuthenticationConfiguration configuration, PasswordEncoder passwordEncoder) throws Exception {
+            this.manager = configuration.getAuthenticationManager();
+            this.encoder = passwordEncoder;
         }
 
         @Override
-        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+            endpoints.authenticationManager(manager)
+                    .tokenStore(tokenStore())
+                    .accessTokenConverter(accessTokenConverter());
+        }
+
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer security) {
+            security.tokenKeyAccess("permitAll()")
+            .checkTokenAccess("isAuthenticated()");
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()
+                    .withClient("application")
+                    .authorizedGrantTypes("client_credentials", "password", "implicit", "authorization_code", "refresh_token")
+                    .redirectUris("https://app.getpostman.com/oauth2/callback")
+                    .secret(encoder.encode("application-secret"))
+                    .scopes("any")
+                    .accessTokenValiditySeconds(3_600);
+        }
+
+        @Bean
+        public JwtAccessTokenConverter accessTokenConverter() {
             JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-            converter.setSigningKey("chave-privada");
-
-            JwtTokenStore store = new JwtTokenStore(converter);
-            endpoints.accessTokenConverter(converter)
-            .tokenStore(store)
-            .authenticationManager(manager);
+            converter.setSigningKey(signingKey);
+            return converter;
         }
 
-
-        @Override
-        public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-            security.tokenKeyAccess("permitAll()");
+        @Bean
+        public TokenStore tokenStore(){
+            return new JwtTokenStore(accessTokenConverter());
         }
     }
 
     @Configuration
     @EnableWebSecurity
-    public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    public static class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetailsService());
+        }
 
         @Bean
-        @Override
         protected UserDetailsService userDetailsService() {
             return new InMemoryUserDetailsManager(
-                User.withDefaultPasswordEncoder()
-                .username("professor")
-                .password("1234")
-                .roles("PROFESSOR")
-                .build(),
-                User.withDefaultPasswordEncoder()
-                .username("estudante")
-                .password("1234")
-                .roles("ESTUDANTE")
-                .build()
+                    User.withUsername("professor")
+                            .password(passwordEncoder().encode("1234"))
+                            .roles("PROFESSOR")
+                            .build(),
+                    User.withUsername("estudante")
+                            .password(passwordEncoder().encode("1234"))
+                            .roles("ESTUDANTE")
+                            .build()
             );
         }
 
+        @Bean
+        public PasswordEncoder passwordEncoder(){
+            return new BCryptPasswordEncoder();
+        }
     }
-
 }
